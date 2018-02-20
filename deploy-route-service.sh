5365aecdd9b9e19c8dd6101cce95f5e155ffa151
benwilcock <benwilcock@gmail.com>
2018-02-20 19:56:15 +0100

    updated the deploy script

--- deploy-route-service.sh
+++ deploy-route-service.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-./gradlew clean assemble
+./gradlew clean check assemble --info
 cf push edge-router-service
-# cf create-user-provided-service edge-router-service -r https://edge-router-service.apps.pcf-t01-we.rabobank.nl
-# cf bind-route-service apps.pcf-t01-we.rabobank.nl edge-router-service --hostname http-header-logger
-curl https://http-header-logger.apps.pcf-t01-we.rabobank.nl/health -H "x-auth-token: BEN:10:COOL" -v
-cf logs edge-router-service --recent
-cf logs http-header-logger --recent
 
+cf create-user-provided-service edge-router-service -r https://edge-router-service.apps.pcf-t01-we.rabobank.nl
+cf bind-route-service apps.pcf-t01-we.rabobank.nl edge-router-service --hostname http-header-logger
+curl https://http-header-logger.apps.pcf-t01-we.rabobank.nl/health -H "x-auth-token: BEN:10:COOL" -v
+
+# cf logs edge-router-service --recent
+# cf logs http-header-logger --recent
 # cf unbind-route-service apps.pcf-t01-we.rabobank.nl edge-router-service --hostname http-header-logger -f
 # cf delete-service edge-router-service -f
 # cf delete edge-router-service -f
